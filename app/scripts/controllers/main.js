db5d7302f7ee35da75586195e3313696f2a1126c
Ulrich Sossou <sorich87@gmail.com>
2013-10-14 21:09:49 +0100

    Generated project structure with AngularJS Yeoman generator

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('bricksApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
