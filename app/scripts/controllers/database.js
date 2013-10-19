fae066990d88a5d0d7341a900c41c983782082d8
Ulrich Sossou <sorich87@gmail.com>
2013-10-19 08:56:23 +0100

    Added database table creation

--- app/scripts/controllers/database.js
+++ app/scripts/controllers/database.js
@@ -1,5 +1,62 @@
 'use strict';
 
 angular.module('bricksApp')
-  .controller('DatabaseCtrl', function ($scope) {
+  .controller('DatabaseCtrl', function ($scope, apps) {
+    $scope.showModal = {newTable: false};
+    $scope.showMenu = {actions: false};
+    $scope.defaultColumns = [
+      {
+        name: 'id',
+        type: 'text'
+      },
+      {
+        name: 'created_at',
+        type: 'date'
+      },
+      {
+        name: 'updated_at',
+        type: 'date'
+      }
+    ];
+
+    // Watch for changes to the current app and set the current table.
+    $scope.appsService = apps;
+    $scope.$watch('appsService.current().id', function () {
+      $scope.app = apps.current();
+
+      if ($scope.app.tables) {
+        $scope.currentTable = $scope.app.tables[0];
+      } else {
+        $scope.app.tables = [];
+      }
+    }, true);
+
+    $scope.hasTables = function () {
+      return $scope.app.tables.length > 0;
+    };
+
+    $scope.selectTable = function (table) {
+      $scope.currentTable = table;
+    };
+
+    $scope.isDefaultColumn = function (column) {
+      var isDefault = false;
+      $scope.defaultColumns.forEach(function (c) {
+        if (c.name === column.name) {
+          isDefault = true;
+        }
+      });
+      return isDefault;
+    };
+
+    // Set the table default columns, add it to the app tables array,
+    // set it as the current table and hide the modal
+    $scope.addTable = function (table) {
+      table.columns = angular.copy($scope.defaultColumns);
+      $scope.app.tables.push(table);
+      apps.update($scope.app);
+      $scope.currentTable = angular.copy(table);
+      $scope.table = {};
+      $scope.showModal.newTable = false;
+    };
   });
