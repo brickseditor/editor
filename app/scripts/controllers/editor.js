0c11a04442ad57f54886efa51d4aa5458c47bf93
Ulrich Sossou <sorich87@gmail.com>
2013-10-23 21:55:26 +0100

    Added pages management

--- app/scripts/controllers/editor.js
+++ app/scripts/controllers/editor.js
@@ -1,13 +1,15 @@
 'use strict';
 
 angular.module('bricksApp')
-  .controller('EditorCtrl', function ($scope, components, pages) {
+  .controller('EditorCtrl', function ($scope, components, apps) {
     $scope.components = components.all();
-    $scope.currentPage = pages.current();
+    $scope.app = apps.current();
+    $scope.newPage = {template: ''};
 
-    $scope.loadPanel = function (type) {
-      $scope.panel = type;
-    };
+    if (!$scope.app.pages || $scope.app.pages.length === 0) {
+      $scope.app.pages = [{url: '/', template: ''}];
+    }
+    $scope.currentPage = $scope.app.pages[0];
 
     // Send received events from one child scope to all children.
     $scope.$on('select', function (e, iframe) {
@@ -18,4 +20,34 @@ angular.module('bricksApp')
       e.stopPropagation();
       $scope.$broadcast('changed');
     });
+
+    $scope.loadPanel = function (type) {
+      $scope.panel = type;
+    };
+
+    $scope.selectPage = function (page) {
+      $scope.currentPage = page;
+    };
+
+    $scope.addPage = function () {
+      var newPage = angular.copy($scope.newPage);
+
+      $scope.app.pages.push(newPage);
+      apps.update($scope.app);
+      $scope.currentPage = newPage;
+
+      $scope.newPage = {template: ''};
+      $scope.showNewPageModal = false;
+    };
+
+    $scope.deletePage = function (page) {
+      $scope.app.pages.some(function (p, i) {
+        if (page.url === p.url) {
+          $scope.app.pages.splice(i, 1);
+          return true;
+        }
+      });
+      apps.update($scope.app);
+      $scope.currentPage = $scope.app.pages[0];
+    };
   });
