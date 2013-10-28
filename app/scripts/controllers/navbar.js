35c10c815d46e2c969bea74a32d574fe0d6131a2
Ulrich Sossou <sorich87@gmail.com>
2013-10-28 05:07:21 +0100

    Added app download

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -1,7 +1,24 @@
 'use strict';
 
 angular.module('bricksApp')
-  .controller('NavbarCtrl', function ($location, $route, $scope, apps) {
+  .controller('NavbarCtrl', function ($http, $location, $route, $scope, apps) {
+    var escape = function (string) {
+      var escapes = {
+        "'":      "'",
+        '\\':     '\\',
+        '\r':     'r',
+        '\n':     'n',
+        '\t':     't',
+        '\u2028': 'u2028',
+        '\u2029': 'u2029'
+      };
+      var escaper = /\\|'|\r|\n|\t|\u2028|\u2029/g;
+
+      return string.replace(escaper, function (match) {
+        return '\\' + escapes[match];
+      });
+    };
+
     $scope.show = $location.path() !== '/start';
     $scope.showAppsMenu = false;
     $scope.showAppsModal = false;
@@ -60,5 +77,38 @@ angular.module('bricksApp')
 
     $scope.hideAppsModal = function () {
       $scope.showAppsModal = false;
+    };
+
+    $scope.download = function () {
+      var zip = new JSZip();
+      var root = zip.folder($scope.currentApp.name);
+
+      $http.get('views/build.html', {cache: true})
+        .then(function (response) {
+          var app = '<script>window.bricksApp = JSON.parse(\'' +
+            escape(JSON.stringify($scope.currentApp)) +
+            '\');</script>';
+          var html = response.data.replace('</head>', app + '\n</head>');
+          root.file('index.html', html);
+
+          return $http.get('bower_components/node-uuid/uuid.js', {
+            cache: true
+          });
+        })
+
+        .then(function (response) {
+          $scope.scripts = response.data;
+
+          return $http.get('scripts/preview.js', {
+            cache: true
+          });
+        })
+
+        .then(function (response) {
+          root.folder('scripts')
+            .file('build.js', $scope.scripts + response.data);
+
+          saveAs(zip.generate({type: 'blob'}), $scope.currentApp.name + '.zip');
+        });
     };
   });
