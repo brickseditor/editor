fa1d6f00d264ad11470a668fce2de7cb5356b5ea
Ulrich Sossou <sorich87@gmail.com>
2013-10-31 15:08:48 +0100

    Changed ng-model and ng-bind to be set by only one field and ng-repeat field to select the table name instead of yes/no

--- app/scripts/ui/components/bindings.js
+++ app/scripts/ui/components/bindings.js
@@ -13,63 +13,22 @@ angular.module('bricksApp.ui')
         scope.tables = apps.current().tables;
         scope.bindings = {};
 
-        var resetBindings = function () {
-          scope.bindings = {
-            column: '',
-            repeat: 'no',
-            table: ''
-          };
-        };
-
-        var parseModel = function (selection) {
-          var  column, model, table;
-
-          model = selection.attr('ng-model') || selection.attr('ng-bind');
-          model = model ? model.split('.') : [];
-
-          if (model.length === 2) {
-            if (!scope.bindings.table) {
-              table = scope.tables.filter(function (t) {
-                return t.name === model[0];
-              })[0];
-              scope.bindings.table = table ? table.name : '';
-            }
-
-            column = table.columns.filter(function (c) {
-              return c.name === model[1];
-            })[0];
-            scope.bindings.column = column ? column.name : '';
-          }
-        };
-
-        var parseRepeat = function (selection) {
-          var repeat = selection.attr('ng-repeat');
+        var parseRepeat = function (repeat) {
           var table;
 
           repeat = repeat ? repeat.split(' ') : [];
 
           if (repeat.length === 3) {
-            scope.bindings.repeat = 'yes';
-
-            if (!scope.bindings.table) {
-              table = scope.tables.filter(function (t) {
-                return t.name === repeat[0];
-              })[0];
-              scope.bindings.table = table ? table.name : '';
-            }
-          }
-        };
-
-        scope.$watch('bindings.table', function (table) {
-          if (scope.tables && table) {
             scope.tables.some(function (t) {
-              if (t.name === table) {
-                scope.columns = t.columns;
+              if (t.name === repeat[0]) {
+                table = t.name;
                 return true;
               }
             });
+
+            return table;
           }
-        });
+        };
 
         // Changes element attributes according to selected bindings.
         scope.$watch('bindings', function (bindings) {
@@ -77,22 +36,14 @@ angular.module('bricksApp.ui')
             return;
           }
 
-          var nodeName = scope.selection.prop('nodeName');
-          var isInput = ['INPUT', 'TEXTAREA', 'SELECT'].indexOf(nodeName) > -1;
-          var attr, repeat;
+          scope.selection.attr('ng-bind', scope.bindings.bind);
+          scope.selection.attr('ng-model', scope.bindings.model);
 
-          if (bindings.repeat === 'yes' && bindings.table) {
-            repeat = bindings.table + ' in data[\'' + bindings.table + '\']';
+          if (bindings.repeat) {
+            var repeat = bindings.repeat + ' in data[\'' + bindings.repeat + '\']';
             scope.selection.attr('ng-repeat', repeat);
           } else {
             scope.selection.removeAttr('ng-repeat');
-          }
-
-          if (bindings.table && bindings.column) {
-            attr = isInput ? 'ng-model' : 'ng-bind';
-            scope.selection.attr(attr, bindings.table + '.' + bindings.column);
-          } else {
-            scope.selection.removeAttr('ng-bind').removeAttr('ng-model');
           }
 
           uiCtrl.updateTemplate();
@@ -101,10 +52,14 @@ angular.module('bricksApp.ui')
         // Parses element attributes to set bindings values.
         scope.$on('selection', function () {
           scope.selection = uiCtrl.selection();
+          scope.nodeName = scope.selection.prop('nodeName');
+          scope.isInput = ['INPUT', 'TEXTAREA', 'SELECT'].indexOf(scope.nodeName) > -1;
 
-          resetBindings();
-          parseModel(scope.selection);
-          parseRepeat(scope.selection);
+          scope.bindings = {
+            bind: scope.selection.attr('ng-bind'),
+            model: scope.selection.attr('ng-model'),
+            repeat: parseRepeat(scope.selection.attr('ng-repeat'))
+          };
 
           $timeout(function () {
             scope.$apply();
