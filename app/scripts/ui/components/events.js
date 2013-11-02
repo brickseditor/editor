4a2332284bd8d633a2fbaf1d2b70391f4cb79403
Ulrich Sossou <sorich87@gmail.com>
2013-11-02 14:12:12 +0100

    Added change event and fixed events directives attributes reading from template

--- app/scripts/ui/components/events.js
+++ app/scripts/ui/components/events.js
@@ -11,28 +11,32 @@ angular.module('bricksApp.ui')
       link: function (scope, element, attrs, uiCtrl) {
         scope.selection = null;
         scope.tables = apps.current().tables;
-        scope.names = ('click dblclick mousedown mouseup mouseover mouseout ' +
-          'mousemove mouseenter mouseleave keydown keyup keypress submit ' +
-          'focus blur copy cut paste').split(' ').sort();
-        scope.events = {};
+        scope.event = {};
+
+        scope.names = (
+          'blur change click copy cut dblclick focus keydown keyup ' +
+          'keypress mousedown mouseenter mouseleave mousemove mouseout ' +
+          'mouseover mouseup paste submit'
+        ).split(' ');
 
         var parseEvent = function (selection, type) {
           var event = selection.attr('ng-' + type);
 
           if (event) {
-            scope.events.type = type;
-            scope.events.action = event.split('(')[0];
-            if (scope.events.action) {
-              scope.events.object = event.split('\'')[1];
+            scope.event.type = type;
+            scope.event.action = event.split('(')[0];
+            if (scope.event.action) {
+              scope.event.object = event.split('\'')[1];
             }
-          } else {
-            scope.events = {};
           }
         };
 
         scope.$on('selection', function () {
           scope.selection = uiCtrl.selection();
-          parseEvent(scope.selection, 'click');
+          scope.event = {};
+          scope.names.forEach(function (event) {
+            parseEvent(scope.selection, event);
+          });
 
           $timeout(function () {
             scope.$apply();
