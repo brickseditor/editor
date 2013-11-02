dd93f637c331a1aecfda1e310bbff7084f06b116
Ulrich Sossou <sorich87@gmail.com>
2013-11-02 13:33:41 +0100

    Made delete and add actions modify app local storage

--- app/scripts/preview.js
+++ app/scripts/preview.js
@@ -39,15 +39,48 @@ angular.module('bricksApp', ['ngRoute'])
   .factory('Storage', function ($window) {
     var Storage = {};
     var app = $window.bricksApp;
+    var data = {};
     var prefix = 'bricks_app_' + app.id + '_';
 
+    var getTable = function (tableName) {
+      var dataString = $window.localStorage.getItem(prefix + tableName);
+      return dataString ? angular.fromJson(dataString) : [];
+    };
+
+    var saveTable = function (tableName) {
+      var dataString = data[tableName] ? angular.toJson(data[tableName]) : '';
+      $window.localStorage.setItem(prefix + tableName, dataString);
+    };
+
+    app.tables.forEach(function (table) {
+      data[table.name] = getTable(table.name);
+    });
+
     Storage.all = function () {
-      var data = {};
-      app.tables.forEach(function (table) {
-        var tableData = $window.localStorage.getItem(prefix + table.name);
-        data[table.name] = tableData ? JSON.parse(tableData) : [];
-      });
       return data;
+    };
+
+    Storage.add = function (tableName, row) {
+      var date = (new Date()).toISOString().split('.')[0].replace('T', ' ');
+
+      row.id = uuid();
+      row.created_at = date;
+      row.updated_at = date;
+
+      data[tableName] = data[tableName] || [];
+      data[tableName].push(row);
+      saveTable(tableName);
+    };
+
+    Storage.delete = function (tableName, row) {
+      if (data[tableName]) {
+        data[tableName].some(function (storedRow, i) {
+          if (storedRow.id === row.id) {
+            data[tableName].splice(i, 1);
+          }
+        });
+        saveTable(tableName);
+      }
     };
 
     return Storage;
@@ -58,23 +91,12 @@ angular.module('bricksApp', ['ngRoute'])
 
     $scope.save = function (table, instance) {
       var row = angular.copy(instance);
-      var date = (new Date()).toISOString().split('.')[0].replace('T', ' ');
-
-      row.id = uuid();
-      row.created_at = date;
-      row.updated_at = date;
-
-      $scope.data[table] = $scope.data[table] || [];
-      $scope.data[table].push(row);
+      Storage.add(table, row);
       $scope[table] = {};
       $scope.submitted = true;
     };
 
     $scope.delete = function (table, instance) {
-      angular.forEach($scope.data[table], function (row, i) {
-        if (row.id === instance.id) {
-          $scope.data[table].splice(i, 1);
-        }
-      });
+      Storage.delete(table, instance);
     };
   });
