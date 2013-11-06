507807afa834e51161c59fd6546830b400762075
Ulrich Sossou <sorich87@gmail.com>
2013-11-06 10:11:06 +0100

    Added Firebase and localStorage in the apps, more components, parent element selection, ngGrid for database rows list, improved ng-{events} support, and other improvements and bug fixes

--- scripts/preview.js
+++ scripts/preview.js
@@ -1,80 +1,48 @@
 'use strict';
 
-angular.module('bricksApp', ['ngRoute'])
+angular.module('bricksApp', ['ngRoute', 'bricksApp.storage'])
   .config(function ($routeProvider) {
-    var template = function (route) {
-      var app = window.bricksApp;
-      var notFound = 'Nothing Found.';
-      var path = route.route ? '/' + route.route : '/';
-      var template;
-
-      if (app.pages.length === 0) {
-        return notFound;
-      }
-
-      app.pages.some(function (p) {
-        if (p.url === path) {
-          template = p.template;
-          return true;
-        }
+    window.bricksApp.pages.forEach(function (page) {
+      $routeProvider.when(page.url, {
+        controller: 'MainCtrl',
+        template: page.template
       });
+    });
 
-      return template ? template : notFound;
-    };
-
-    var params = {
-      template: template,
-      controller: 'MainCtrl'
-    };
-
-    $routeProvider
-      .when('/', params)
-      .when('/:route*', params);
+    $routeProvider.otherwise({
+      template: 'Nothing Found.'
+    });
   })
 
   .run(function ($window) {
     angular.element('#bricksAppStyle').html($window.bricksApp.css);
   })
 
-  .factory('Storage', function ($window) {
-    var Storage = {};
-    var app = $window.bricksApp;
-    var prefix = 'bricks_app_' + app.id + '_';
+  .controller('MainCtrl', function ($routeParams, $scope, $window, Storage) {
+    var routeKeys = Object.keys($routeParams);
+    var storage = new Storage($window.bricksApp);
 
-    Storage.all = function () {
-      var data = {};
-      app.tables.forEach(function (table) {
-        var tableData = $window.localStorage.getItem(prefix + table.name);
-        data[table.name] = tableData ? JSON.parse(tableData) : [];
+    $scope.data = storage.all();
+
+    if (routeKeys.length > 0) {
+      routeKeys.forEach(function (table) {
+        $scope[table] = storage.get(table, $routeParams[table]) || {};
       });
-      return data;
-    };
-
-    return Storage;
-  })
-
-  .controller('MainCtrl', function ($scope, Storage) {
-    $scope.data = Storage.all();
+    }
 
     $scope.save = function (table, instance) {
-      var row = angular.copy(instance);
-      var date = (new Date()).toISOString().split('.')[0].replace('T', ' ');
-
-      row.id = uuid();
-      row.created_at = date;
-      row.updated_at = date;
-
-      $scope.data[table] = $scope.data[table] || [];
-      $scope.data[table].push(row);
+      if (instance.id) {
+        storage.update(table, instance);
+      } else {
+        storage.add(table, angular.copy(instance));
+      }
       $scope[table] = {};
       $scope.submitted = true;
     };
 
     $scope.delete = function (table, instance) {
-      angular.forEach($scope.data[table], function (row, i) {
-        if (row.id === instance.id) {
-          $scope.data[table].splice(i, 1);
-        }
-      });
+      storage.remove(table, instance);
     };
+
+    eval($window.bricksApp.js);
   });
