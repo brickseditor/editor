17bb31a49c8742dd3fee56ef975834aaf74d2cb8
Ulrich Sossou <sorich87@gmail.com>
2013-11-08 11:20:37 +0100

    Made plenty of small improvements and bug fixes

--- scripts/preview.js
+++ scripts/preview.js
@@ -5,7 +5,12 @@ angular.module('bricksApp', ['ngRoute', 'bricksApp.storage'])
     window.bricksApp.pages.forEach(function (page) {
       $routeProvider.when(page.url, {
         controller: 'MainCtrl',
-        template: page.template
+        template: page.template,
+        resolve: {
+          storage: ['$window', 'Storage', function ($window, Storage) {
+            return Storage.init($window.bricksApp);
+          }]
+        }
       });
     });
 
@@ -18,9 +23,8 @@ angular.module('bricksApp', ['ngRoute', 'bricksApp.storage'])
     angular.element('#bricksAppStyle').html($window.bricksApp.css);
   })
 
-  .controller('MainCtrl', function ($routeParams, $scope, $window, Storage) {
+  .controller('MainCtrl', function ($location, $parse, $routeParams, $scope, $window, storage) {
     var routeKeys = Object.keys($routeParams);
-    var storage = new Storage($window.bricksApp);
 
     $scope.data = storage.all();
 
@@ -30,18 +34,37 @@ angular.module('bricksApp', ['ngRoute', 'bricksApp.storage'])
       });
     }
 
-    $scope.save = function (table, instance) {
-      if (instance.id) {
-        storage.update(table, instance);
-      } else {
-        storage.add(table, angular.copy(instance));
+    $scope.add = function (table, instance, e) {
+      if (e) {
+        e.preventDefault();
       }
+
+      storage.add(table, angular.copy(instance));
       $scope[table] = {};
-      $scope.submitted = true;
     };
 
-    $scope.delete = function (table, instance) {
+    $scope.update = function (table, instance, e) {
+      if (e) {
+        e.preventDefault();
+      }
+
+      storage.update(table, instance);
+    };
+
+    $scope.remove = function (table, instance, e) {
+      if (e) {
+        e.preventDefault();
+      }
+
       storage.remove(table, instance);
+    };
+
+    $scope.visit = function (url, e) {
+      if (e) {
+        e.preventDefault();
+      }
+
+      $location.path(url);
     };
 
     eval($window.bricksApp.js);
--- scripts/storage/storage.js
+++ scripts/storage/storage.js
@@ -2,8 +2,8 @@
 
 angular.module('bricksApp.storage', ['firebase'])
 
-  .factory('localData', function ($rootScope, $window) {
-    var scope = $rootScope.$new();
+  .factory('localData', function ($q, $window) {
+    var data = {};
     var prefix;
 
     var getTable = function (tableName) {
@@ -12,15 +12,16 @@ angular.module('bricksApp.storage', ['firebase'])
     };
 
     var saveTable = function (tableName) {
-      var dataString = scope.data[tableName] ?
-        angular.toJson(scope.data[tableName]) :
+      var dataString = data[tableName] ?
+        angular.toJson(data[tableName]) :
         '';
       $window.localStorage.setItem(prefix + tableName, dataString);
     };
 
-    scope.data = {};
+    return function (app, scope) {
+      var deferred = $q.defer();
 
-    return function (app) {
+      data = scope.data;
       prefix = 'bricks_app_' + app.id + '_';
 
       app.tables.forEach(function (table) {
@@ -31,40 +32,43 @@ angular.module('bricksApp.storage', ['firebase'])
         }, true);
       });
 
-      return scope.data;
+      deferred.resolve();
+
+      return deferred.promise;
     }
   })
 
   .factory('firebaseData', function ($rootScope, angularFire) {
-    return function (app) {
-      var scope = $rootScope.$new();
-
-      scope.data = {};
-
-      app.tables.forEach (function (table) {
-        scope.data[table.name] = [];
-
-        angularFire(new Firebase(
-          'https://' + app.settings.firebase + '.firebaseio.com/' + table.name
-        ), scope, 'data.' + table.name);
-      });
-
-      return scope.data;
+    return function (app, scope) {
+      return angularFire(new Firebase(
+        'https://' + app.settings.firebase + '.firebaseio.com'
+      ), scope, 'data');
     }
   })
 
-  .factory('Storage', function (firebaseData, localData) {
+  .service('Storage', function ($q, $rootScope, firebaseData, localData) {
+    var Storage = {};
     var data;
 
-    var Storage = function (app) {
+    Storage.init = function (app) {
+      var scope = $rootScope.$new();
+      var promise;
+
+      scope.data = {};
+
       if (app.storage === 'firebase') {
-        data = firebaseData(app);
+        promise = firebaseData(app, scope);
       } else {
-        data = localData(app);
+        promise = localData(app, scope);
       }
+
+      return promise.then(function () {
+        data = scope.data;
+        return Storage;
+      });
     };
 
-    Storage.prototype.all = function (tableName) {
+    Storage.all = function (tableName) {
       if (tableName) {
         return data[tableName];
       } else {
@@ -72,7 +76,7 @@ angular.module('bricksApp.storage', ['firebase'])
       }
     };
 
-    Storage.prototype.get = function (tableName, id) {
+    Storage.get = function (tableName, id) {
       var row;
 
       if (data[tableName]) {
@@ -87,7 +91,7 @@ angular.module('bricksApp.storage', ['firebase'])
       return row;
     };
 
-    Storage.prototype.add = function (tableName, row) {
+    Storage.add = function (tableName, row) {
       var date = (new Date()).toISOString().split('.')[0].replace('T', ' ');
 
       row.id = uuid();
@@ -98,7 +102,7 @@ angular.module('bricksApp.storage', ['firebase'])
       data[tableName].push(row);
     };
 
-    Storage.prototype.update = function (tableName, row) {
+    Storage.update = function (tableName, row) {
       if (data[tableName]) {
         data[tableName].some(function (storedRow, i) {
           if (storedRow.id === row.id) {
@@ -109,7 +113,7 @@ angular.module('bricksApp.storage', ['firebase'])
       }
     };
 
-    Storage.prototype.remove = function (tableName, row) {
+    Storage.remove = function (tableName, row) {
       if (data[tableName]) {
         data[tableName].some(function (storedRow, i) {
           if (storedRow.id === row.id) {
@@ -120,7 +124,7 @@ angular.module('bricksApp.storage', ['firebase'])
       }
     };
 
-    Storage.prototype.clear = function (tableName) {
+    Storage.clear = function (tableName) {
       data[tableName].length = 0;
     };
 
